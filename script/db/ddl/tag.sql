5ca954603767476699da44d251b58dbf732ee241
LZx <l.mikespace@outlook.com>
2023-07-19 23:18:55 +0800

    add create_at update_at col

--- script/db/ddl/tag.sql
+++ script/db/ddl/tag.sql
@@ -7,6 +7,7 @@
 CREATE TABLE public.tag (
 	id uuid NOT NULL,
 	"name" varchar NOT NULL,
-	create_time timestamptz NULL,
+	create_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	update_at timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT tag_pk PRIMARY KEY (id)
 );

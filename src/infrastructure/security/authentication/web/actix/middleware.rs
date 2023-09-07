c73c0723aef55bd7146fa907adf0589c92adbb79
LZx <l.mikespace@outlook.com>
2023-09-07 23:59:29 +0800

    remove futures-util

--- src/infrastructure/security/authentication/web/actix/middleware.rs
+++ src/infrastructure/security/authentication/web/actix/middleware.rs
@@ -1,34 +1,33 @@
-use std::future::{ready, Ready};
+use std::future::{ready, Future, Ready};
+use std::pin::Pin;
+use std::rc::Rc;
 use std::task::{Context, Poll};
 
-use actix_web::body::{EitherBody, MessageBody};
+use actix_web::body::MessageBody;
 use actix_web::dev::{Service, ServiceRequest, ServiceResponse, Transform};
 use actix_web::Error;
 
 pub struct AuthenticateStatusService<S> {
-    service: S,
+    service: Rc<S>,
 }
 
 impl<S, B> Service<ServiceRequest> for AuthenticateStatusService<S>
 where
     S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
     S::Future: 'static,
-    B: MessageBody + 'static,
+    B: MessageBody,
 {
-    type Response = ServiceResponse<EitherBody<B>>;
+    type Response = ServiceResponse<B>;
     type Error = S::Error;
-    type Future = Ready<Result<Self::Response, Self::Error>>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
 
     fn poll_ready(&self, ctx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.service.poll_ready(ctx)
     }
 
     fn call(&self, req: ServiceRequest) -> Self::Future {
-        println!("----------");
-        async {
-            let map = self.service.call(req).await;
-        };
-        todo!()
+        let service = Rc::clone(&self.service);
+        Box::pin(async move { service.call(req).await })
     }
 }
 
@@ -38,15 +37,17 @@ impl<S, B> Transform<S, ServiceRequest> for AuthenticateStateTransform
 where
     S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
     S::Future: 'static,
-    B: MessageBody + 'static,
+    B: MessageBody,
 {
-    type Response = ServiceResponse<EitherBody<B>>;
+    type Response = ServiceResponse<B>;
     type Error = S::Error;
     type Transform = AuthenticateStatusService<S>;
     type InitError = ();
     type Future = Ready<Result<Self::Transform, Self::InitError>>;
 
     fn new_transform(&self, service: S) -> Self::Future {
-        ready(Ok(AuthenticateStatusService { service }))
+        ready(Ok(AuthenticateStatusService {
+            service: Rc::new(service),
+        }))
     }
 }

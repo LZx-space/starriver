7f68b07e0eedf9fd05920e7e1f0630f246a9b13b
LZx <l.mikespace@outlook.com>
2023-09-18 00:21:46 +0800

    update article dll

--- script/db/ddl/ddl_article.sql
+++ script/db/ddl/ddl_article.sql
@@ -1,16 +1,23 @@
--- public.article definition
-
--- Drop table
-
--- DROP TABLE public.article;
-
-CREATE TABLE public.article (
-	id uuid NOT NULL,
-	title varchar NULL,
-	body text NULL,
-	author_id varchar NULL,
-	create_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	update_at timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
-	CONSTRAINT article_pk PRIMARY KEY (id)
+create table public.article
+(
+    id        uuid                                               not null
+        primary key,
+    title     varchar,
+    body      text,
+    state     smallint,
+    author_id varchar,
+    create_at timestamp with time zone default CURRENT_TIMESTAMP not null,
+    update_at timestamp with time zone default CURRENT_TIMESTAMP
 );
-CREATE INDEX idx_article_title ON public.article USING btree (title);
+
+comment on column public.article.title is '标题';
+comment on column public.article.body is '正文';
+comment on column public.article.state is '状态';
+comment on column public.article.author_id is '作者';
+
+alter table public.article
+    owner to postgres;
+
+create index idx_article_title
+    on public.article (title);
+

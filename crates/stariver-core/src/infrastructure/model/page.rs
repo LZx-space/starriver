40bed13fc9d04b7e2d450110ba6f5a2efc652bd3
LZx <l.mikespace@outlook.com>
2024-04-19 19:06:11 +0800

    calc total_pages

--- crates/stariver-core/src/infrastructure/model/page.rs
+++ crates/stariver-core/src/infrastructure/model/page.rs
@@ -23,10 +23,14 @@ pub struct PageResult<T: Serialize> {
 
 impl<T: Serialize> PageResult<T> {
     pub fn new(page: u64, page_size: u64, total_items: u64, items: Vec<T>) -> Self {
+        let mut total_pages = total_items / page_size;
+        if total_items % page_size != 0 {
+            total_pages = total_pages + 1;
+        }
         PageResult {
             page,
             page_size,
-            total_pages: 0,
+            total_pages,
             total_items,
             items,
         }

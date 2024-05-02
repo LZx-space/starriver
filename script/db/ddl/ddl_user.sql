a6af8db7cd8ef7be6d6d21deb3f257d19c9759da
LZx <l.mikespace@outlook.com>
2024-05-02 22:53:36 +0800

    user api

---
+++ script/db/ddl/ddl_user.sql
@@ -0,0 +1,8 @@
+create table public."user"
+(
+    id        uuid primary key         not null,
+    username  character varying(100)   not null,
+    password  character varying(100)   not null,
+    create_at timestamp with time zone not null default CURRENT_TIMESTAMP,
+    update_at timestamp with time zone          default CURRENT_TIMESTAMP
+);

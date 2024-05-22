39b62c904a2af6665582a987166795e3928cff9f
LZx <l.mikespace@outlook.com>
2024-05-22 15:27:42 +0800

    test mod

--- crates/stariver-core/src/infrastructure/security/authentication/util.rs
+++ crates/stariver-core/src/infrastructure/security/authentication/util.rs
@@ -18,24 +18,29 @@ pub fn verify_password(password: &str, password_hash_string: PasswordHashString)
     argon2.verify_password(password.as_bytes(), &password_hash_string.password_hash())
 }
 
-#[test]
-pub fn argon2_test() {
-    use argon2::password_hash::SaltString;
-    use argon2::{Argon2, PasswordHasher, PasswordVerifier};
+mod test {
     use std::time::SystemTime;
 
-    let start = SystemTime::now();
-    let argon2 = Argon2::default();
-    let pwd_bytes = "password".as_bytes();
-    let salt_string = SaltString::encode_b64("ABCDEFGH".as_bytes()).expect("啊啊啊");
-    let hash = argon2
-        .hash_password(pwd_bytes, &salt_string)
-        .expect("啵啵啵");
-    let duration = SystemTime::now().duration_since(start).expect("啊啊啊");
-    println!("{:?}", duration);
-    let result = argon2.verify_password(pwd_bytes, &hash);
-    println!("{:?}", result);
-    let hash_string = hash.serialize();
-    let hash_str = hash_string.as_str();
-    println!("{}", hash_str);
+    use argon2::password_hash::SaltString;
+    use argon2::{Argon2, PasswordHasher, PasswordVerifier};
+    use uuid::Uuid;
+
+    #[test]
+    pub fn argon2_test() {
+        println!("{}", Uuid::now_v7().to_string());
+        let start = SystemTime::now();
+        let argon2 = Argon2::default();
+        let pwd_bytes = "123".as_bytes();
+        let salt_string = SaltString::encode_b64("stariver".as_bytes()).expect("啊啊啊");
+        let hash = argon2
+            .hash_password(pwd_bytes, &salt_string)
+            .expect("啵啵啵");
+        let duration = SystemTime::now().duration_since(start).expect("啊啊啊");
+        println!("{:?}", duration);
+        let result = argon2.verify_password(pwd_bytes, &hash);
+        println!("{:?}", result);
+        let hash_string = hash.serialize();
+        let hash_str = hash_string.as_str();
+        println!("{}", hash_str);
+    }
 }

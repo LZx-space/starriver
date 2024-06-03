ca7513ffa291427f55fb9d1f2c7da0d5ed5be7fd
LZx <l.mikespace@outlook.com>
2024-06-03 16:18:32 +0800

    remove crate async_static

--- crates/stariver-core/src/infrastructure/util/db.rs
+++ crates/stariver-core/src/infrastructure/util/db.rs
@@ -1,19 +1,14 @@
 use std::env;
+use std::sync::OnceLock;
 
-use async_static::async_static;
 use sea_orm::{Database, DatabaseConnection};
 
-async_static! {
-    static ref CONN: DatabaseConnection = init_db_conn().await;
-}
+static DB_CONN: OnceLock<DatabaseConnection> = OnceLock::new();
 
 pub async fn db_conn() -> &'static DatabaseConnection {
-    CONN.await
-}
-
-async fn init_db_conn() -> DatabaseConnection {
     let db_url = env::var("DB_URL").expect("DB_URL environment variable not set");
-    Database::connect(db_url)
+    let conn = Database::connect(db_url)
         .await
-        .expect("create a DatabaseConnection failed")
+        .expect("create a DatabaseConnection failed");
+    DB_CONN.get_or_init(|| conn)
 }

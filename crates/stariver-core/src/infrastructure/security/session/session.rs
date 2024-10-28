7cee398ee058ec71f074ca5365f52f0e1417fa66
LZx <l.mikespace@outlook.com>
2024-10-29 00:10:31 +0800

    add cargo sha2 & hmac

--- crates/stariver-core/src/infrastructure/security/session/session.rs
+++ crates/stariver-core/src/infrastructure/security/session/session.rs
@@ -71,3 +71,56 @@ pub struct Claims {
     nbf: usize, // Optional. Not Before (as UTC timestamp)
     sub: String, // Optional. Subject (whom token refers to)
 }
+
+#[cfg(test)]
+mod test {
+    use hmac::{Hmac, Mac};
+    use redis::Commands;
+    use sha2::Sha256;
+    use std::time::Instant;
+
+    #[test]
+    pub fn test_redis() {
+        // connect to redis
+        let client =
+            redis::Client::open("redis://127.0.0.1/").expect("failed to create redis client");
+        let mut con = client
+            .get_connection()
+            .expect("failed to get redis connection");
+        let _: () = con.set("my_key", 42).expect("failed to set value");
+
+        let start = Instant::now();
+        let val: i64 = con.get("my_key").expect("failed to get my_key");
+        println!("{}", val);
+        let end = Instant::now();
+        println!("代码执行时间: {:?} 微秒", (end - start).as_micros());
+    }
+
+    #[test]
+    pub fn test_hmac() {
+        type HmacSha256 = Hmac<Sha256>;
+
+        let mut mac = HmacSha256::new_from_slice(b"my secret and secure key")
+            .expect("HMAC can take key of any size");
+        mac.update(b"input message input message nput message input message nput message input message nput message input message nput message input message nput message input message nput message input message nput message input message");
+
+        // `result` has type `CtOutput` which is a thin wrapper around array of
+        // bytes for providing constant time equality check
+        let result = mac.finalize();
+        // To get underlying array use `into_bytes`, but be careful, since
+        // incorrect use of the code value may permit timing attacks which defeats
+        // the security provided by the `CtOutput`
+        let code_bytes = result.into_bytes();
+        let start = Instant::now();
+        let mut mac = HmacSha256::new_from_slice(b"my secret and secure key")
+            .expect("HMAC can take key of any size");
+
+        mac.update(b"input message input message nput message input message nput message input message nput message input message nput message input message nput message input message nput message input message nput message input message");
+
+        // `verify_slice` will return `Ok(())` if code is correct, `Err(MacError)` otherwise
+        mac.verify_slice(&code_bytes[..])
+            .expect("HMAC verification failed");
+        let end = Instant::now();
+        println!("代码执行时间: {:?} 微秒", (end - start).as_micros());
+    }
+}

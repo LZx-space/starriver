5f51c62d3ce5b0cc0924afefee788b188a0c6fe7
LZx <l.mikespace@outlook.com>
2025-01-01 20:19:33 +0800

    add authentication error

--- crates/stariver-core/src/infrastructure/security/authentication/core/authenticator.rs
+++ crates/stariver-core/src/infrastructure/security/authentication/core/authenticator.rs
@@ -23,6 +23,15 @@ pub enum AuthenticationError {
     #[error("username not found")]
     UsernameNotFound,
 
+    #[error("username is empty")]
+    UsernameEmpty,
+
+    #[error("password not found")]
+    PasswordNotFound,
+
+    #[error("password is empty")]
+    PasswordEmpty,
+
     #[error("bad password")]
     BadPassword,
 

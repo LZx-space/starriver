a17eb79342e82d7564f9499d59a779c912424742
LZx <l.mikespace@outlook.com>
2025-01-01 18:20:33 +0800

    remove unessential fn

--- crates/stariver-core/src/infrastructure/security/authentication/core/credential.rs
+++ crates/stariver-core/src/infrastructure/security/authentication/core/credential.rs
@@ -1,10 +1,6 @@
 /// 用户凭证<br>
 /// 典型的: 用户名&密码、OAuth2访问令牌
 pub trait Credential {
-    type Id;
-
-    fn id(&self) -> &Self::Id;
-
     /// 请求的详情，有时候需要用其来辅助认证的过程
     fn request_details(&self) -> RequestDetails;
 }

746b62f23f9ed3700c97835dee781ef7b4055c31
LZx <l.mikespace@outlook.com>
2025-01-01 20:19:48 +0800

    rename

--- crates/stariver-core/src/infrastructure/security/authentication/core/credential.rs
+++ crates/stariver-core/src/infrastructure/security/authentication/core/credential.rs
@@ -1,10 +1,10 @@
-/// 用户凭证<br>
-/// 典型的: 用户名&密码、OAuth2访问令牌
+/// 用于认证的凭证<br>
+/// 举例: 用户名&密码、OAuth2访问令牌
 pub trait Credential {
     /// 请求的详情，有时候需要用其来辅助认证的过程
-    fn request_details(&self) -> RequestDetails;
+    fn request_details(&self) -> Ctx;
 }
 
 /// 认证请求的详情，通常是记录HTTP请求中的信息
 #[derive(Debug)]
-pub struct RequestDetails {}
+pub struct Ctx {}

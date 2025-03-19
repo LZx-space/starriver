960edb0988eef9ebbb3da94ae42da20e5552df76
LZx <l.mikespace@outlook.com>
2025-03-19 23:11:44 +0800

    polish

--- crates/stariver-core/src/infrastructure/util/db.rs
+++ crates/stariver-core/src/infrastructure/util/db.rs
@@ -1,11 +1,13 @@
+use sea_orm::{Database, DatabaseConnection};
 use std::env;
 use std::sync::OnceLock;
-
-use sea_orm::{Database, DatabaseConnection};
 
 static DB_CONN: OnceLock<DatabaseConnection> = OnceLock::new();
 
 pub async fn db_conn() -> &'static DatabaseConnection {
+    if let Some(conn) = DB_CONN.get() {
+        return conn;
+    }
     let db_url = env::var("DB_URL").expect("DB_URL environment variable not set");
     let conn = Database::connect(db_url)
         .await

e6353feafd1dd1449755ba8bc2c4acdbe93250db
LZx <l.mikespace@outlook.com>
2026-03-07 14:27:29 +0800

    polish doc comment

--- crates/starriver-infrastructure/src/macros/sea_orm_active_model_macro.rs
+++ crates/starriver-infrastructure/src/macros/sea_orm_active_model_macro.rs
@@ -1,9 +1,11 @@
 /// 为了减少SQL操作字段；对比 ActiveModel 与聚合根（或实体）的属性值，不同才调用其 Set 方法
 ///
-/// 参数说明：
+/// # 参数说明：
 /// - $active_model: 待更新字段值的ActiveModel
 /// - $entity: 聚合根/实体的实例
 /// - $($field:ident),*: 需要对比的字段列表
+/// # 返回值
+/// bool: 是否有字段执行了Set操作
 #[macro_export]
 macro_rules! update_active_model_on_change {
     ($active_model:expr, $entity:expr, $($field:ident),*) => {{

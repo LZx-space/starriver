2c17d1f9b4543444b79e84cf9ab22d521e2eca29
LZx <l.mikespace@outlook.com>
2026-03-09 13:02:45 +0800

    show script dir

--- script/wsl_local_db_pg_init.bat
+++ script/wsl_local_db_pg_init.bat
@@ -1,1 +1,8 @@
+@echo off
+chcp 65001 >nul 2>&1
+
+set "script_path=%~dp0"
+echo 当前脚本所在的绝对路径：%script_path%
+cd /d "%script_path%"
+
 wsl psql -h localhost -p 5432 -U postgres -d postgres -f ./db/db_init.sql

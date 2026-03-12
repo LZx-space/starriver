4a6bc8a1aaae83da90c82b2fbfc86c084abdb2a4
LZx <l.mikespace@outlook.com>
2026-03-12 16:11:27 +0800

    modify comment

--- crates/starriver-infrastructure/src/security/authentication/core/credentials.rs
+++ crates/starriver-infrastructure/src/security/authentication/core/credentials.rs
@@ -1,5 +1,5 @@
 /// 用于认证的凭证
 ///
 /// * 举例: 用户名&密码、OAuth2访问令牌
-/// * 学术上 credential 在用户名密码登录场景下一般指password，但复数也会习惯来指两者
+/// * 学术上 credential 一般指用户所声称 identity 的凭据。但其复数可以表达所有用于认证的信息
 pub trait Credentials: Send + Sync {}

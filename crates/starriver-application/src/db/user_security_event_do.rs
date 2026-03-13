d97049b2df45b1ea07100c15caa4028d9dcd5dc9
LZx <l.mikespace@outlook.com>
2026-03-13 18:52:25 +0800

    add sea-orm model relation

--- crates/starriver-application/src/db/user_security_event_do.rs
+++ crates/starriver-application/src/db/user_security_event_do.rs
@@ -16,6 +16,19 @@ pub struct Model {
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::user_do::Entity",
+        from = "Column::UserId",
+        to = "super::user_do::Column::Id"
+    )]
+    User,
+}
+
+impl Related<super::user_do::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::User.def()
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {}

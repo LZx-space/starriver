f52c08ddc38bf9b1866a9fb212601115831411d8
LZx <l.mikespace@outlook.com>
2026-03-19 00:31:41 +0800

    add fkey

--- script/db/ddl/03_create_table_user_security_event.sql
+++ script/db/ddl/03_create_table_user_security_event.sql
@@ -1,11 +1,16 @@
 CREATE TABLE public.user_security_event
 (
-    id         uuid primary key         NOT NULL,
+    id         uuid                     NOT NULL,
     user_id    uuid                     NOT NULL,
     event_type smallint                 NOT NULL,
     message    character varying(100)   NOT NULL,
     create_at  timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_at  timestamp with time zone
+    update_at  timestamp with time zone,
+    CONSTRAINT user_security_event_pkey PRIMARY KEY (id),
+    CONSTRAINT user_security_event_fkey FOREIGN KEY (user_id)
+        REFERENCES public."user" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
 );
 
 COMMENT ON TABLE public.user_security_event

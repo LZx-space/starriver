82ff275055b1c84fc7f7bb1088c353065729d1d0
LZx <l.mikespace@outlook.com>
2026-03-21 23:10:40 +0800

    email service

--- crates/starriver-infrastructure/src/service.rs
+++ crates/starriver-infrastructure/src/service.rs
@@ -1,3 +1,3 @@
-// todo 放在infra是稳健的，但是作用域是否过大，提升进application?
 pub mod dictionary;
+pub mod email_service;
 pub mod weather_service;
---
+++ crates/starriver-infrastructure/src/service/email_service.rs
@@ -0,0 +1,82 @@
+use std::env;
+
+use lettre::{
+    AsyncSmtpTransport, AsyncTransport, Message, Tokio1Executor,
+    transport::smtp::{Error, authentication::Credentials, response::Response},
+};
+
+pub struct EmailClient {
+    smtp_client: AsyncSmtpTransport<Tokio1Executor>,
+}
+
+impl EmailClient {
+    pub fn new(cfg: StmpConfig) -> Result<Self, Error> {
+        let creds = Credentials::new(cfg.username, cfg.password);
+        let smtp_client = AsyncSmtpTransport::<Tokio1Executor>::starttls_relay(&cfg.host)?
+            .port(cfg.port)
+            .credentials(creds)
+            .build();
+        Ok(Self { smtp_client })
+    }
+
+    pub fn with_env() -> Result<Self, Error> {
+        let username =
+            env::var("EMAIL_USERNAME").expect("EMAIL_USERNAME environment variable not set");
+        let password =
+            env::var("EMAIL_PASSWORD").expect("EMAIL_PASSWORD environment variable not set");
+        let host = env::var("EMAIL_HOST").expect("EMAIL_HOST environment variable not set");
+        let port = env::var("EMAIL_PORT")
+            .expect("EMAIL_PORT environment variable not set")
+            .parse::<u16>()
+            .expect("EMAIL_PORT environment variable must be a valid port number");
+        Self::new(StmpConfig {
+            host,
+            port,
+            username,
+            password,
+        })
+    }
+
+    /// 发送邮件
+    pub async fn send(&self, message: Message) -> Result<Response, Error> {
+        self.smtp_client.send(message).await
+    }
+
+    /// 检查连接是否正常
+    pub async fn test_conn(&self) -> Result<bool, Error> {
+        self.smtp_client.test_connection().await
+    }
+}
+
+pub struct StmpConfig {
+    ///邮箱服务器主机
+    pub host: String,
+    /// 邮箱服务器端口
+    pub port: u16,
+    /// 邮箱地址
+    pub username: String,
+    /// 邮箱密码
+    pub password: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[ignore]
+    #[tokio::test]
+    async fn test_send() {
+        dotenvy::dotenv().expect(".env file not found");
+        let client = EmailClient::with_env();
+        assert_eq!(
+            client.is_ok(),
+            true,
+            "测试邮件服务器连接失败，创建客户端失败"
+        );
+        let result = client.unwrap().test_conn().await;
+        assert!(result.is_ok(), "测试邮件服务器连接失败，连接异常");
+        let ok = result.unwrap();
+        assert!(ok, "测试邮件服务器连接失败，未返回OK");
+        assert_eq!(ok, true, "测试邮件服务器连接失败，返回OK，但其值不为真");
+    }
+}

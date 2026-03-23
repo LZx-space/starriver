bb8d8e8740288c8975ab2d107f3571fcaa34ba20
LZx <l.mikespace@outlook.com>
2026-03-23 23:40:52 +0800

    add axum-valid for params valid

--- crates/starriver-adapter/src/api/user_handler.rs
+++ crates/starriver-adapter/src/api/user_handler.rs
@@ -2,6 +2,7 @@ use crate::config::app_state::AppState;
 use axum::Json;
 use axum::extract::State;
 use axum::response::IntoResponse;
+use axum_valid::Valid;
 use starriver_application::user_dto::{EmailVerifyCmd, UserCmd};
 use starriver_infrastructure::security::authentication::_default_impl::AuthenticatedUser;
 
@@ -11,12 +12,16 @@ pub async fn me(user: AuthenticatedUser) -> impl IntoResponse {
 
 pub async fn register_inactive_user(
     state: State<AppState>,
-    cmd: Json<UserCmd>,
+    cmd: Valid<Json<UserCmd>>,
 ) -> impl IntoResponse {
-    let cmd = cmd.0;
+    let cmd = cmd.into_inner().0;
     state.user_application.register_user(cmd).await
 }
 
-pub async fn verify_email(state: State<AppState>, cmd: Json<EmailVerifyCmd>) -> impl IntoResponse {
-    state.user_application.send_verification_email(cmd.0).await
+pub async fn verify_email(
+    state: State<AppState>,
+    cmd: Valid<Json<EmailVerifyCmd>>,
+) -> impl IntoResponse {
+    let cmd = cmd.into_inner().0;
+    state.user_application.send_verification_email(cmd).await
 }

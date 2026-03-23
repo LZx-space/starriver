5341a037f77ab27426595a70e3a7712d87e7446e
LZx <l.mikespace@outlook.com>
2026-03-23 23:40:52 +0800

    add axum-valid for params valid

--- crates/starriver-application/src/dto/user_dto.rs
+++ crates/starriver-application/src/dto/user_dto.rs
@@ -1,14 +1,18 @@
 use serde::Deserialize;
+use validator::Validate;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Validate)]
 pub struct UserCmd {
     pub username: String,
     pub password: String,
+    #[validate(email)]
     pub email: String,
+    #[validate(length(equal = 6))]
     pub email_code: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Validate)]
 pub struct EmailVerifyCmd {
+    #[validate(email)]
     pub email: String,
 }

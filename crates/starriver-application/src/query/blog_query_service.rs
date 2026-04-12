c873d872eee0173a9301bca47676843cd4a2e6e3
LZx <l.mikespace@outlook.com>
2026-04-12 15:53:46 +0800

    add blog content excerptor

--- crates/starriver-application/src/query/blog_query_service.rs
+++ crates/starriver-application/src/query/blog_query_service.rs
@@ -1,8 +1,9 @@
 use crate::db::blog_do::{Column, Entity};
-use sea_orm::{DatabaseConnection, EntityTrait, PaginatorTrait, QuerySelect, prelude::Expr};
+use sea_orm::{DatabaseConnection, EntityTrait, PaginatorTrait, QuerySelect};
 use starriver_infrastructure::{
     error::ApiError,
     model::page::{PageQuery, PageResult},
+    util::html_utils::{DefaultExcerptor, Excerptor},
 };
 
 use crate::blog_dto::res::BlogExcerpt;
@@ -23,14 +24,25 @@ impl BlogQueryService for DefaultBlogQueryService {
     async fn find_page(&self, q: PageQuery) -> Result<PageResult<BlogExcerpt>, ApiError> {
         let blogs = Entity::find()
             .select_only()
-            .columns([Column::Id, Column::Title, Column::State, Column::CreateAt])
-            .column_as(Expr::cust("SUBSTRING(content, 1, 100)"), Column::Content)
+            .columns([
+                Column::Id,
+                Column::Title,
+                Column::Content,
+                Column::State,
+                Column::CreateAt,
+            ])
             .offset(q.page * q.page_size)
             .limit(q.page_size)
             .into_model::<BlogExcerpt>()
             .all(&self.conn)
             .await
-            .map_err(ApiError::from)?;
+            .map_err(ApiError::from)?
+            .into_iter()
+            .map(|mut e| {
+                e.excerpt = DefaultExcerptor::excerpt(&e.excerpt, 200);
+                e
+            })
+            .collect::<Vec<_>>();
         let record_total = Entity::find()
             .select_only()
             .column(Column::Id)
--- crates/starriver-infrastructure/src/util.rs
+++ crates/starriver-infrastructure/src/util.rs
@@ -1,4 +1,5 @@
 pub mod db;
 pub mod file_utils;
+pub mod html_utils;
 pub mod jwt_utils;
 pub mod regex_patterns;

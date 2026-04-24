07ec0334dec9112f3f4d236b911d91e2af6dc06e
LZx <l.mikespace@outlook.com>
2026-04-24 20:49:53 +0800

    fmt

--- script/db/ddl/03_create_table_user_security_event.sql
+++ script/db/ddl/03_create_table_user_security_event.sql
@@ -1,13 +1,13 @@
 CREATE TABLE public.user_security_event
 (
-    id         uuid                     NOT NULL,
-    user_id    uuid                     NOT NULL,
-    event_type smallint                 NOT NULL,
-    message    character varying(100)   NOT NULL,
+    id          uuid                     NOT NULL,
+    user_id     uuid                     NOT NULL,
+    event_type  smallint                 NOT NULL,
+    message     character varying(100)   NOT NULL,
     created_at  timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at  timestamp with time zone,
-    CONSTRAINT user_security_event_pkey PRIMARY KEY (id),
-    CONSTRAINT user_security_event_fkey FOREIGN KEY (user_id)
+    CONSTRAINT  user_security_event_pkey PRIMARY KEY (id),
+    CONSTRAINT  user_security_event_fkey FOREIGN KEY (user_id)
         REFERENCES public."user" (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE CASCADE
